45eedb90baffcc8ffb68765145302e326cd58946
Hagen Hentschel <hagen.hentschel@protonmail.ch>
2021-09-17 00:09:47 +0200

    allow building frappe-nginx locally
    there is no arm64 image in the registry

---
+++ build_images.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ERPNEXT_VERSION=v12
+FRAPPE_VERSION=version-12
+
+docker build --build-arg=GIT_BRANCH=$FRAPPE_VERSION -t frappe-nginx:$ERPNEXT_VERSION . --file build/frappe-nginx/Dockerfile
+#docker build --build-arg=FRAPPE_BRANCH=$VERSION -t custom-erpnext-nginx:$TAG build/nginx
+#docker build --build-arg=FRAPPE_BRANCH=$VERSION -t custom-erpnext-worker:$TAG build/worker
