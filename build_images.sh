ced7a9fb9e26d7665ff0fc3ee72d9d72e8113d61
Hagen Hentschel <hagen.hentschel@protonmail.ch>
2021-09-17 00:26:14 +0200

    allow building erpnext-nginx locally
    there is no arm64 image in the registry

--- build_images.sh
+++ build_images.sh
@@ -4,5 +4,6 @@ ERPNEXT_VERSION=v12
 FRAPPE_VERSION=version-12
 
 docker build --build-arg=GIT_BRANCH=$FRAPPE_VERSION -t frappe-nginx:$ERPNEXT_VERSION . --file build/frappe-nginx/Dockerfile
+docker build --build-arg=IMAGE_TAG=$ERPNEXT_VERSION --build-arg=GIT_BRANCH=$FRAPPE_VERSION . --file build/erpnext-nginx/Dockerfile
 #docker build --build-arg=FRAPPE_BRANCH=$VERSION -t custom-erpnext-nginx:$TAG build/nginx
 #docker build --build-arg=FRAPPE_BRANCH=$VERSION -t custom-erpnext-worker:$TAG build/worker
